864d9a70489cad9df7c1107f558869966df50027
yutooo323 <yuto.morine03@gmail.com>
2021-06-13 14:18:25 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '03b157070d099f15da3da876ddb56a15be56c5cb54eb122f248e6fc7926aafd48599ae00dc1df93a24e1e704d702a18086ce8fd6947b4ff049bf6f78d44a3b0f'
